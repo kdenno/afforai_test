e5b801d2066ae4c48e3a30d6ccea7964ff8e0280
Dennis Kabuye <dennis@nebula.io>
2023-12-12 08:10:15 +0300

    create button and typography components

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import { Typography } from './Typography';
+import PropTypes from 'prop-types'
 
-export function Button() {
+const BUTTON_VARIANTS = {
+  primary: {
+    background: 'linear-gradient(135deg,#6004ff,#7f35ff 23.95%,#370097 60.11%,#8d4aff)',
+    border: '1px solid hsla(0,0%,100%,.2)',
+    borderRadius: '8px',
+    color: '#fff',
+    padding: '5px 14px',
+    textAlign: 'center',
+    cursor: 'pointer'
+  },
+  secondary: {
+    backgroundColor: '#f5f5f5',
+    border: '1px solid #e6e6e6',
+    borderRadius: '8px',
+    color: '#525252',
+    padding: '5px 14px',
+    textAlign: 'center',
+    cursor: 'pointer'
+  },
+ 
+};
+
+export function Button(props) {
+  const { onClick, disabled, btnText, variant = 'secondary', small, sx, ...restProps } = props;
+  const customSx = sx || null;
+
   return (
-    <div>Button</div>
+    <button
+      onClick={onClick}
+      style={{...BUTTON_VARIANTS[variant], ...customSx}}
+      disabled={disabled}
+      {...restProps}
+    >
+      <Typography variant={small ? 'bodySmall' : 'titleSmall500'}>
+        {btnText}
+      </Typography>
+    </button>
   )
+  
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  small: PropTypes.bool,
+  btnText: PropTypes.string,
+  sx: PropTypes.object,
+  variant: PropTypes.oneOf(Object.keys(BUTTON_VARIANTS)),
 }
--- src/layouts/navBar/Navbar.jsx
+++ src/layouts/navBar/Navbar.jsx
@@ -1,48 +1,53 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styles from './navBar.module.css';
+import { Button } from '../../components/ui/Button';
+import { Typography } from '../../components/ui/Typography';
 
 export function Navbar() {
-    const [showMobileMenu, setShowMobileMenu] = useState(false)
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
 
     return (
-    <div className={styles['navWrapper']}>
-        <nav className={styles['nav']}>
-            <div className={styles['desktopMenu']}>
-                <div className={styles['logo']}>
-                    <picture aria-label="site-logo"> 
-                        <source srcset="/images/logo-new-violet.png" />
-                        <img src="/images/logo-new-violet.png" height="25" width="25" alt="site-logo" />
-                    </picture>
-                    <div className={styles['title']}>Afforai</div>
+        <div className={styles['navWrapper']}>
+            <nav className={styles['nav']}>
+                <div className={styles['desktopMenu']}>
+                    <div className={styles['logo']}>
+                        <picture aria-label="site-logo"> 
+                            <source srcSet="/images/logo-new-violet.png" />
+                            <img src="/images/logo-new-violet.png" height="25" width="25" alt="site-logo" />
+                        </picture>
+                        <div className={styles['title']}>
+                            <Typography variant='smallHeadline500'>Afforai</Typography>
+                        </div>
+                    </div>
+                    <div className={styles['navLinks']}>
+                        <ul>
+                        <li><a href="#">Affiliate</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li>Testimonials</li>
+                        <li>Help Center</li>
+                        </ul>
+                    </div>
+                    <div className={styles['navActions']}>
+                        <Button aria-label="login" btnText="Log in" sx={{marginRight: 8}} />
+                        <Button aria-label="try-for-free" btnText="Try for free" variant="primary" />
+                        <button aria-label="toggle-mobile-menu" className={styles['mobileMenuToggler']}
+                        onClick={() => setShowMobileMenu(!showMobileMenu)}
+                        >MM</button>
+                    </div>
                 </div>
-                <div className={styles['navLinks']}>
-                    <ul>
-                    <li class="crumb"><a href="#">Affiliate</a></li>
-                    <li class="crumb"><a href="#">Pricing</a></li>
-                    <li class="crumb">Testimonials</li>
-                    <li class="crumb">Help Center</li>
-                    </ul>
-                </div>
-                <div className={styles['navActions']}>
-                    <button aria-label="login">Log in</button>
-                    <button aria-label="try-for-free">Try for free</button>
-                    <button className={styles['mobileMenuToggler']}
-                     onClick={() => setShowMobileMenu(!showMobileMenu)}
-                    >MM</button>
-                </div>
-            </div>
-            <div 
-             className={styles['mobilenavLinks']}
-             style={{display: `${showMobileMenu ? 'block' : 'none'}`}}
-             >
-                    <ul>
-                    <li class="crumb"><a href="#">Affiliate</a></li>
-                    <li class="crumb"><a href="#">Pricing</a></li>
-                    <li class="crumb">Testimonials</li>
-                    <li class="crumb">Help Center</li>
-                    </ul>
-                </div>
+                <div 
+                className={styles['mobilenavLinks']}
+                style={{display: `${showMobileMenu ? 'block' : 'none'}`}}
+                >
+                        <ul>
+                        <li><a href="#">Affiliate</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li>Testimonials</li>
+                        <li>Help Center</li>
+                        </ul>
+                    </div>
 
-        </nav>
-    </div>)
+            </nav>
+        </div>
+    )
 }
