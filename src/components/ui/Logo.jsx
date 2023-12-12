233af73f99699e2a0ad2727125dbd3658fb65041
Dennis Kabuye <dennis@nebula.io>
2023-12-12 17:17:06 +0300

    create footer

---
+++ src/components/ui/Logo.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import styles from './uiComponents.module.css';
+import { LinkElement } from './LinkElement'
+import { Typography } from './Typography'
+
+export function Logo() {
+  return (
+    <div className={styles['logo']}>
+
+        <picture aria-label="site-logo"> 
+            <source srcSet="/images/logo-new-violet.png" />
+            <img src="/images/logo-new-violet.png" height="25" width="25" alt="site-logo" />
+        </picture>
+
+        <div className={styles['title']}>
+            <LinkElement to='/' aria-label="to-home-page">
+                <Typography variant='smallHeadline500'>Afforai</Typography>
+            </LinkElement>
+        </div>
+
+    </div>
+  )
+}
